3db60862656e82cf2d55ebb6c41a3adf3dce001a
Victor Costan <costan@gmail.com>
2012-03-02 22:50:31 +0100

    Full path classification.

--- police-vminfo/lib/police/vminfo/paths.rb
+++ police-vminfo/lib/police/vminfo/paths.rb
@@ -1,24 +1,45 @@
 module Police
   
 module VmInfo
-  # True if the given path belongs to a gem.
+  # True if the given source code path belongs to a gem.
   def self.gem_path?(path)
-    Gem.default_path.any? do |gem_path|
-      next false if path.length < gem_path.length
-      if path.length > gem_path.length
-        path[0, gem_path.length] == gem_path &&
-            path[gem_path.length] == File::SEPARATOR
-      else
-        path == gem_path
+    Gem.default_path.any? { |gem_path| Paths.descendant? path, gem_path }
+  end
+  
+  # True if the given source code path belongs to the Ruby standard library.
+  def self.stdlib_path?(path)
+    # NOTE: assuming the convention that all directories are prepended to the
+    #       load path throughout a program's execution
+    load_paths = $LOAD_PATH    
+    last_gem_index = 0
+    (load_paths.length - 1).downto(0) do |index|
+      if gem_path? load_paths[index]
+        last_gem_index = index
+        break
       end
     end
-    paths = $:
-    (0...paths.first.length).each do |i|
-      
-    end
+    stdlib_paths = load_paths[((last_gem_index || -1) + 1)..-1]
+    stdlib_paths.any? { |stdlib_path| Paths.descendant? path, stdlib_path }
   end
-  def self.stdlib_path
-    
+  
+  # True if the given source code path belongs to the Ruby VM kernel.
+  def self.kernel_path?(path)
+    !$LOAD_PATH.any? { |load_path| Paths.descendant? path, load_path }
+  end
+  
+  # Implementation details.  
+  module Paths
+    class <<self
+      # True if a path points to a descendant of the directory in anothe
+      def descendant?(descendant, dir)
+        if descendant.length > dir.length
+          descendant[0, dir.length] == dir &&
+              descendant[dir.length] == File::SEPARATOR
+        else
+          descendant == dir
+        end
+      end
+    end
   end
 end  # module VmInfo
 
---
+++ police-vminfo/test/vminfo/paths_test.rb
@@ -0,0 +1,69 @@
+require File.expand_path('../helper.rb', File.dirname(__FILE__))
+require 'net/http'
+
+describe Police::VmInfo do
+  def some_method; end
+
+  APP_PATH = Police::VmInfo.method(:gem_path?).source_location.first
+  TEST_PATH = self.instance_method(:some_method).source_location.first
+  HTTP_PATH = Net::HTTP.method(:new).source_location.first
+  BUNDLER_PATH = Bundler.method(:setup).source_location.first
+  
+  describe 'gem_path?' do
+    it 'accepts Bundler path' do
+      Police::VmInfo.gem_path?(BUNDLER_PATH).must_equal true
+    end
+
+    it 'rejects Net::HTTP path' do
+      Police::VmInfo.gem_path?(HTTP_PATH).must_equal false
+    end
+    
+    it 'rejects test path' do
+      Police::VmInfo.gem_path?(TEST_PATH).must_equal false
+    end
+
+    it 'rejects app path' do
+      Police::VmInfo.gem_path?(APP_PATH).must_equal false
+    end
+  end
+  
+  describe 'stdlib_path?' do
+    it 'rejects Bundler path' do
+      Police::VmInfo.stdlib_path?(BUNDLER_PATH).must_equal false
+    end
+
+    it 'accepts Net::HTTP path' do
+      Police::VmInfo.stdlib_path?(HTTP_PATH).must_equal true
+    end
+    
+    it 'rejects test path' do
+      Police::VmInfo.stdlib_path?(TEST_PATH).must_equal false
+    end
+
+    it 'rejects app path' do
+      Police::VmInfo.stdlib_path?(APP_PATH).must_equal false
+    end
+  end
+  
+  describe 'kernel_path?' do
+    it 'rejects Bundler path' do
+      Police::VmInfo.kernel_path?(BUNDLER_PATH).must_equal false
+    end
+
+    it 'rejects Net::HTTP path' do
+      Police::VmInfo.kernel_path?(HTTP_PATH).must_equal false
+    end
+    
+    it 'rejects test path' do
+      Police::VmInfo.kernel_path?(TEST_PATH).must_equal false
+    end
+
+    it 'rejects app path' do
+      Police::VmInfo.kernel_path?(APP_PATH).must_equal false
+    end
+    
+    it 'accepts a Rubinius kernel path' do
+      Police::VmInfo.kernel_path?('kernel/bootstrap/string.rb').must_equal true
+    end
+  end
+end
